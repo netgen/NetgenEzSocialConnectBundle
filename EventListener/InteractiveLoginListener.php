6200143c6bd04574526a622eeba6d3f78af86eb0
Ivan Herak <ivan@netgen.hr>
2015-06-18 14:46:32 +0200

    Refractored code, added helper

--- EventListener/InteractiveLoginListener.php
+++ EventListener/InteractiveLoginListener.php
@@ -2,51 +2,37 @@
 
 namespace Netgen\Bundle\EzSocialConnectBundle\EventListener;
 
-use eZ\Publish\API\Repository\UserService;
-use eZ\Publish\API\Repository\Exceptions\InvalidArgumentException;
-use eZ\Publish\API\Repository\Repository;
 use eZ\Publish\Core\MVC\Symfony\Event\InteractiveLoginEvent;
 use eZ\Publish\Core\MVC\Symfony\MVCEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\ChainConfigResolver;
 use eZ\Publish\API\Repository\Values\User\User;
 use eZ\Publish\API\Repository\Exceptions\NotFoundException;
 use Doctrine\ORM\EntityManagerInterface;
 use eZ\Publish\Core\Helper\FieldHelper;
 use Netgen\Bundle\EzSocialConnectBundle\Entity\OAuthEz;
+use Netgen\Bundle\EzSocialConnectBundle\OAuth\OAuthEzUser;
+use Netgen\Bundle\EzSocialConnectBundle\Helper\SocialLoginHelper;
 
 class InteractiveLoginListener implements EventSubscriberInterface
 {
-    /**
-     * @var \eZ\Publish\API\Repository\Repository $repository
-     */
-    protected $repository;
-
-    /** @var  \eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\ConfigResolver $configResolver */
-    protected $configResolver;
-
-    /** @var int $userGroup */
-    protected $userGroup;
-
     /** @var  FieldHelper */
     protected $fieldHelper;
 
     /** @var  EntityManagerInterface */
     protected $entityManager;
 
+    /** @var SocialLoginHelper SocialLoginHelper */
+    protected $loginHelper;
+
     public function __construct(
-        Repository $repository,
-        ChainConfigResolver $configResolver,
-        $userGroup,
         FieldHelper $fieldHelper,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        SocialLoginHelper $loginHelper
     )
     {
-        $this->repository = $repository;
-        $this->configResolver = $configResolver;
-        $this->userGroup = $userGroup;
         $this->fieldHelper = $fieldHelper;
         $this->entityManager = $entityManager;
+        $this->loginHelper = $loginHelper;
     }
 
     public static function getSubscribedEvents()
@@ -63,58 +49,32 @@ class InteractiveLoginListener implements EventSubscriberInterface
      */
     public function onInteractiveLogin( InteractiveLoginEvent $event )
     {
-        $userService = $this->repository->getUserService();
-
-        /** @var \Netgen\Bundle\EzSocialConnectBundle\OAuth\OAuthEzUser $oauthUser */
+        /** @var OAuthEzUser $oauthUser */
         $oauthUser = $event->getAuthenticationToken()->getUser();
+        $imageLink = $oauthUser->getImageLink();
 
-        $loginId = $oauthUser->getOriginalId();
-        $username = $oauthUser->getUsername();
-        $password = md5( $loginId . $username );
-        $email = $oauthUser->getEmail();
-        $first_name = $oauthUser->getFirstName();
-        $last_name = $oauthUser->getLastName();
-        $resourceOwnerName = $oauthUser->getResourceOwnerName();
-        $imageLink = $oauthUser->getImagelink();
+        /** @var OAuthEz $oauthEzUserEntity */
+        $oauthEzUserEntity = $this->loginHelper->loadFromTable( $oauthUser );
 
-        $OAuthEzEntity =
-            $this->entityManager
-                ->getRepository( 'NetgenEzSocialConnectBundle:OAuthEz' )
-                ->findBy(
-                    array(
-                        'resourceUserId' => $loginId,
-                        'resourceName' => $resourceOwnerName
-                    ),
-                    array(
-                        'ezUserId' => 'DESC' //get last inserted
-                    )
-                );
-
-        if( !empty( $OAuthEzEntity ) )
+        if( !empty( $oauthEzUserEntity ) )
         {
             try
             {
+                $ezUserId = $oauthEzUserEntity->getEzUserId();
                 /** @var User $user */
-                $user = $userService->loadUser( $OAuthEzEntity[ 0 ]->getEzUserId() );
+                $user = $this->loginHelper->loadEzUserById( $ezUserId );
 
-                if ( $this->fieldHelper->isFieldEmpty( $user->content, "image" ) && !empty( $imageLink ) )
+                if ( $this->fieldHelper->isFieldEmpty( $user->content, "image" ) &&
+                    !empty( $imageLink ) )
                 {
-                    $this->addProfileImage( $user, $imageLink );
+                    $this->loginHelper->addProfileImage( $user, $imageLink );
                 }
 
-                if( $email !== $user->email )
+                if( $oauthUser->getEmail() !== $user->email )
                 {
                     try
                     {
-                        $this->repository->setCurrentUser(
-                            $userService->loadUserByLogin("admin")
-                        );
-
-                        $userUpdateStruct = $userService->newUserUpdateStruct();
-                        $userUpdateStruct->email = $email;
-                        $userService->updateUser( $user, $userUpdateStruct );
-
-                        $this->repository->setCurrentUser( $user );
+                        $this->loginHelper->updateUserFields( $user, array( "email" => $oauthUser->getEmail() ) );
                     }
                     catch( \Exception $e )
                     {
@@ -131,117 +91,12 @@ class InteractiveLoginListener implements EventSubscriberInterface
             {
                 // something went wrong - data is in the table, but the user does not exist
                 // remove falty data and fallback to creating new user
-                $this->entityManager->remove( $OAuthEzEntity[0] );
-                $this->entityManager->flush();
+                $this->loginHelper->removeFromTable( $oauthEzUserEntity );
             }
         }
 
-        $this->repository->setCurrentUser(
-            $userService->loadUserByLogin( "admin" )
-        );
-
-        $contentType = $this->repository->getContentTypeService()->loadContentTypeByIdentifier( "user" );
-        $languages = $this->configResolver->getParameter( 'languages' );
-
-        $userCreateStruct = $userService->newUserCreateStruct(
-            $username,
-            $email,
-            $password,
-            $languages[ 0 ],
-            $contentType
-        );
-        if ( !empty( $first_name ) )
-        {
-            $userCreateStruct->setField( 'first_name', $first_name );
-        }
-        if ( !empty( $last_name ) )
-        {
-            $userCreateStruct->setField( 'last_name', $last_name );
-        }
-
-        $imageFileName = null;
-        if ( !empty( $imageLink ) )
-        {
-            $imageFileName = $this->downloadExternalImage( $imageLink );
-
-            // upload image to ez
-            $userCreateStruct->setField( 'image', $imageFileName );
-        }
-
-        // Created user needs to be enabled
-        $userCreateStruct->enabled = true;
-
-        $userGroup = $userService->loadUserGroup( $this->userGroup[ $resourceOwnerName ] );
-
-        $user = $userService->createUser(
-            $userCreateStruct,
-            array( $userGroup )
-        );
-
-        // add to table
-        $OAuthEzEntity = new OAuthEz();
-        $OAuthEzEntity
-            ->setEzUserId( $user->id )
-            ->setResourceUserId( $loginId )
-            ->setResourceName( $resourceOwnerName );
-
-        $this->entityManager->persist( $OAuthEzEntity );
-        $this->entityManager->flush();
-
-        $this->repository->setCurrentUser( $user );
+        $user = $this->loginHelper->createEzUser( $oauthUser );
+        $this->loginHelper->addToTable( $user, $oauthUser );
         $event->setApiUser( $user );
-    }
-
-    protected function downloadExternalImage( $imageLink )
-    {
-        // download image from facebook
-        $data = file_get_contents( $imageLink );
-
-        preg_match("/.+\.(jpg|png|jpeg|gif)/", $imageLink, $imageName );
-
-        if( !empty($imageName[0]) )
-        {
-            $storageDir = '/tmp/';
-            if( !is_dir( $storageDir ) )
-            {
-                if( !mkdir( $storageDir ) )
-                {
-                    throw new \Exception('Failed to create dir');
-                }
-            }
-            $imageFileName = $storageDir . basename( $imageName[ 0 ] );
-        }
-        if( !empty( $imageFileName ) && file_put_contents( $imageFileName, $data ) )
-        {
-            \eZLog::write( 'Local image created ' . $imageFileName );
-
-            return $imageFileName;
-        }
-        else
-        {
-            \eZLog::write( 'Problem while saving image ' . $imageLink );
-
-            return null;
-        }
-    }
-
-    protected function addProfileImage( $user, $imageLink )
-    {
-        $userService = $this->repository->getUserService();
-
-        $this->repository->setCurrentUser(
-            $userService->loadUserByLogin("admin")
-        );
-        $imageFileName = $this->downloadExternalImage( $imageLink );
-        $contentService = $this->repository->getContentService();
-
-        $languages = $this->configResolver->getParameter( 'languages' );
-        $userDraft = $contentService->createContentDraft( $user->content->versionInfo->contentInfo );
-        $userUpdateStruct = $contentService->newContentUpdateStruct();
-        $userUpdateStruct->initialLanguageCode = $languages[0];
-        $userUpdateStruct->setField( 'image', $imageFileName );
-        $userDraft = $contentService->updateContent( $userDraft->versionInfo, $userUpdateStruct );
-
-        $contentService->publishVersion( $userDraft->versionInfo );
     }
 } 
---
+++ Helper/SocialLoginHelper.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace Netgen\Bundle\EzSocialConnectBundle\Helper;
+
+use eZ\Publish\API\Repository\Repository;
+use Doctrine\ORM\EntityManagerInterface;
+use eZ\Publish\Core\MVC\ConfigResolverInterface;
+use eZ\Publish\API\Repository\Values\User\User;
+use Netgen\Bundle\EzSocialConnectBundle\OAuth\OAuthEzUser;
+use Netgen\Bundle\EzSocialConnectBundle\Entity\OAuthEz;
+
+class SocialLoginHelper
+{
+    /** @var Repository Repository */
+    protected $repository;
+
+    /** @var  EntityManagerInterface */
+    protected $entityManager;
+
+    /** @var  ConfigResolverInterface */
+    protected $configResolver;
+
+    /** @var array $userGroup */
+    protected $userGroup;
+
+
+    public function __construct(
+        Repository $repository,
+        EntityManagerInterface $entityManager,
+        ConfigResolverInterface $configResolver,
+        array $userGroup
+    )
+    {
+        $this->repository = $repository;
+        $this->entityManager = $entityManager;
+        $this->configResolver = $configResolver;
+        $this->userGroup = $userGroup;
+    }
+
+    public function downloadExternalImage( $imageLink )
+    {
+        // download image from facebook
+        $data = file_get_contents( $imageLink );
+
+        preg_match("/.+\.(jpg|png|jpeg|gif)/", $imageLink, $imageName );
+
+        if( !empty($imageName[0]) )
+        {
+            $storageDir = '/tmp/';
+            if( !is_dir( $storageDir ) )
+            {
+                if( !mkdir( $storageDir ) )
+                {
+                    throw new \Exception('Failed to create dir');
+                }
+            }
+            $imageFileName = $storageDir . basename( $imageName[ 0 ] );
+        }
+        if( !empty( $imageFileName ) && file_put_contents( $imageFileName, $data ) )
+        {
+            \eZLog::write( 'Local image created ' . $imageFileName );
+
+            return $imageFileName;
+        }
+        else
+        {
+            \eZLog::write( 'Problem while saving image ' . $imageLink );
+
+            return null;
+        }
+    }
+
+    public function addProfileImage( $user, $imageLink )
+    {
+        $userService = $this->repository->getUserService();
+
+        $this->repository->setCurrentUser(
+            $userService->loadUserByLogin("admin")
+        );
+        $imageFileName = $this->downloadExternalImage( $imageLink );
+        $contentService = $this->repository->getContentService();
+
+        $languages = $this->configResolver->getParameter( 'languages' );
+        $userDraft = $contentService->createContentDraft( $user->content->versionInfo->contentInfo );
+        $userUpdateStruct = $contentService->newContentUpdateStruct();
+        $userUpdateStruct->initialLanguageCode = $languages[0];
+        $userUpdateStruct->setField( 'image', $imageFileName );
+        $userDraft = $contentService->updateContent( $userDraft->versionInfo, $userUpdateStruct );
+
+        $contentService->publishVersion( $userDraft->versionInfo );
+    }
+
+    public function addToTable( User $user, OAuthEzUser $authEzUser )
+    {
+        $OAuthEzEntity = new OAuthEz();
+        $OAuthEzEntity
+            ->setEzUserId( $user->id )
+            ->setResourceUserId( $authEzUser->getOriginalId() )
+            ->setResourceName( $authEzUser->getResourceOwnerName() );
+
+        $this->entityManager->persist( $OAuthEzEntity );
+        $this->entityManager->flush();
+    }
+
+    public function removeFromTable( OAuthEz $userEntity )
+    {
+        $this->entityManager->remove( $userEntity );
+        $this->entityManager->flush();
+    }
+
+    public function loadFromTable( OAuthEzUser $oauthUser )
+    {
+        $results =
+            $this->entityManager
+                ->getRepository( 'NetgenEzSocialConnectBundle:OAuthEz' )
+                ->findBy(
+                    array(
+                        'resourceUserId' => $oauthUser->getOriginalId(),
+                        'resourceName' => $oauthUser->getResourceOwnerName()
+                    ),
+                    array(
+                        'ezUserId' => 'DESC' //get last inserted
+                    )
+                );
+
+        if( !is_array( $results ) || empty( $results ) )
+        {
+            return null;
+        }
+
+        return $results[0];
+    }
+
+    public function createEzUser( OAuthEzUser $oauthUser )
+    {
+        $userService = $this->repository->getUserService();
+
+        $this->repository->setCurrentUser(
+            $userService->loadUserByLogin( "admin" )
+        );
+
+        $loginId = $oauthUser->getOriginalId();
+        $username = $oauthUser->getUsername();
+        $password = md5( $loginId . $username );
+        $first_name = $oauthUser->getFirstName();
+        $last_name = $oauthUser->getLastName();
+        $imageLink = $oauthUser->getImagelink();
+
+        $contentType = $this->repository->getContentTypeService()->loadContentTypeByIdentifier( "user" );
+        $languages = $this->configResolver->getParameter( 'languages' );
+
+        $userCreateStruct = $userService->newUserCreateStruct(
+            $username,
+            $oauthUser->getEmail(),
+            $password,
+            $languages[ 0 ],
+            $contentType
+        );
+        if ( !empty( $first_name ) )
+        {
+            $userCreateStruct->setField( 'first_name', $first_name );
+        }
+        if ( !empty( $last_name ) )
+        {
+            $userCreateStruct->setField( 'last_name', $last_name );
+        }
+
+        $imageFileName = null;
+        if ( !empty( $imageLink ) )
+        {
+            $imageFileName = $this->downloadExternalImage( $imageLink );
+            $userCreateStruct->setField( 'image', $imageFileName );
+        }
+
+        $userCreateStruct->enabled = true;
+
+        $userGroup = $userService->loadUserGroup( $this->userGroup[ $oauthUser->getResourceOwnerName() ] );
+
+        $user = $userService->createUser(
+            $userCreateStruct,
+            array( $userGroup )
+        );
+
+        $this->repository->setCurrentUser( $user );
+
+        return $user;
+    }
+
+    public function updateUserFields( User $user, $fields )
+    {
+        $userService = $this->repository->getUserService();
+
+        $this->repository->setCurrentUser(
+            $this->repository->getUserService()->loadUserByLogin("admin")
+        );
+
+        $userUpdateStruct = $userService->newUserUpdateStruct();
+        foreach( $fields as $name => $value )
+        {
+            $userUpdateStruct->$name = $value;
+        }
+        $userService->updateUser( $user, $userUpdateStruct );
+
+        $this->repository->setCurrentUser( $user );
+    }
+
+    public function loadEzUserById( $userId )
+    {
+        return $this->repository->getUserService()->loadUser( $userId );
+    }
+}
