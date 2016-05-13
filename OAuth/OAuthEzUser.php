1a8741269a5cd3cfd84a3e834e4a487dd0138232
Filip Božanović <filip@netgen.hr>
2016-05-13 14:37:48 +0200

    Add docblocks to OAuthEzUser for consistency

--- OAuth/OAuthEzUser.php
+++ OAuth/OAuthEzUser.php
@@ -29,61 +29,97 @@ class OAuthEzUser extends OAuthUser
         $this->originalId = $id;
     }
 
+    /**
+     * @param $id
+     */
     public function setOriginalId($id)
     {
         $this->originalId = $id;
     }
 
+    /**
+     * @return string
+     */
     public function getOriginalId()
     {
         return $this->originalId;
     }
 
+    /**
+     * @param $name
+     */
     public function setFirstName($name)
     {
         $this->first_name = $name;
     }
 
+    /**
+     * @return string
+     */
     public function getFirstName()
     {
         return $this->first_name;
     }
 
+    /**
+     * @param $surname
+     */
     public function setLastName($surname)
     {
         $this->last_name = $surname;
     }
 
+    /**
+     * @return string
+     */
     public function getLastName()
     {
         return $this->last_name;
     }
 
+    /**
+     * @param $email
+     */
     public function setEmail($email)
     {
         $this->email = $email;
     }
 
+    /**
+     * @return string
+     */
     public function getEmail()
     {
         return $this->email;
     }
 
+    /**
+     * @param $resourceOwnerName
+     */
     public function setResourceOwnerName($resourceOwnerName)
     {
         $this->resourceOwnerName = $resourceOwnerName;
     }
 
+    /**
+     * @return string
+     */
     public function getResourceOwnerName()
     {
         return $this->resourceOwnerName;
     }
 
+    /**
+     * @param $imageLink
+     */
     public function setImageLink($imageLink)
     {
         $this->imageLink = $imageLink;
     }
 
+    /**
+     * @return string
+     */
     public function getImageLink()
     {
         return $this->imageLink;
